438a8c4a6f01ed1080d08614f90b0ce49669d8a7
syed-amer-gulzar <146704595+syed-amer-gulzar@users.noreply.github.com>
2023-10-03 11:18:05 +0500

    Add files via upload

---
+++ task.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+
+# Load the Iris dataset
+iris = datasets.load_iris()
+data = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+target = pd.Series(iris.target, name='target')
+
+# Select the feature and target variable
+X = data['sepal length (cm)'].values.reshape(-1, 1)
+y = target
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a linear regression model
+model = LinearRegression()
+
+# Train the model on the training data
+model.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f'Mean Squared Error: {mse:.2f}')
+# print(f'R-squared (Coefficient of Determination): {r2:.2f}')
+print("Hello world")
+
+plt.scatter(X_test, y_test, color='black')
+plt.plot(X_test, y_pred, color='blue', linewidth=3)
+plt.xlabel('Sepal Length (cm)')
+plt.ylabel('Target')
+plt.title('Linear Regression on Sepal Length')
+plt.show()
